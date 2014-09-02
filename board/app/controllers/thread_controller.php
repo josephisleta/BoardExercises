c2ea27b035d8a35f8724c463b2a2cdd69d294453
joseph <asdasdsd>
2014-09-02 10:20:22 +0800

    Thread Viewing

--- board/app/controllers/thread_controller.php
+++ board/app/controllers/thread_controller.php
@@ -8,4 +8,12 @@ class ThreadController extends AppController
 		
 		$this->set(get_defined_vars());
 	}
+
+	public function view()
+	{
+		$thread = Thread::get(Param::get('thread_id'));
+		$comments = $thread->getComments();
+
+		$this->set(get_defined_vars());
+	}
 }
